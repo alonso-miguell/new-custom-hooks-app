66f144caf583698731bf5cbd2c7ff11d952fda0d
alonso <alonsomiguel.wpc@gmail.com>
2026-02-19 21:36:55 -0600

    Tailwind setUp

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+// import {NewReactApp} from "./NewReactApp.tsx";
+import {TrafficLight} from "./TrafficLight.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/*<NewReactApp />*/}
+      <TrafficLight></TrafficLight>
   </StrictMode>,
 )
