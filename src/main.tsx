f0dc7677b922798419575696804a6d2a43e808d7
alonso <alonsomiguel.wpc@gmail.com>
2026-02-28 12:07:36 -0600

    Added game with suffledWords using basic hooks

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import {PokemonPage} from "./PokemonPage.tsx";
 import {FocusScreen} from "./hooks/useRef/FocusScreen.tsx";
 import {TasksApp} from "@/hooks/useReducer/TaskApp.tsx";
 import {TasksAppWithReducer} from "@/hooks/useReducer/TaskAppWithReducer.tsx";
+import {ScrambleWords} from "@/ScrambleWords.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       {/*<PokemonPage></PokemonPage>*/}
       {/*<FocusScreen></FocusScreen>*/}
       {/*<TasksApp></TasksApp>*/}
-      <TasksAppWithReducer></TasksAppWithReducer>
+      {/*<TasksAppWithReducer></TasksAppWithReducer>*/}
+      <ScrambleWords></ScrambleWords>
   </StrictMode>,
 )
