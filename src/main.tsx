9ed533654fba3273d10f412e480086ca3235e69f
alonso <alonsomiguel.wpc@gmail.com>
2026-03-02 21:09:21 -0600

    Added memoHook files

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import {TasksApp} from "@/hooks/useReducer/TaskApp.tsx";
 import {TasksAppWithReducer} from "@/hooks/useReducer/TaskAppWithReducer.tsx";
 import {ScrambleWords} from "@/hooks/useReducer/ScrambleWords.tsx";
 import {ScrambleWordsWithReducer} from "@/hooks/useReducer/ScrambleWordsWithReducer.tsx";
+import {MemoHook} from "@/hooks/memos/MemoHook.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')!).render(
       {/*<FocusScreen></FocusScreen>*/}
       {/*<TasksApp></TasksApp>*/}
       {/*<TasksAppWithReducer></TasksAppWithReducer>*/}
-      <ScrambleWordsWithReducer></ScrambleWordsWithReducer>
+      {/*<ScrambleWordsWithReducer></ScrambleWordsWithReducer>*/}
+      <MemoHook></MemoHook>
   </StrictMode>,
 )
